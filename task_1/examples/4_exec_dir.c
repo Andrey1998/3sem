b6d278949e58054985991820c52406651e19f5ad
Andrey1998 <32199925+Andrey1998@users.noreply.github.com>
2017-09-22 20:50:34 +0400

    task 1

--- task_1/examples/4_exec_dir.c
+++ task_1/examples/4_exec_dir.c
@@ -4,6 +4,7 @@
 
 #include <unistd.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
@@ -11,9 +12,20 @@ int main() {
    	int status, died;
    	switch(pid = fork()) {
    		case 0: 
-			execl("/bin/ls","ls","-l",NULL); // this is the code the child runs 
-   		default: 
+		{
+			execl("/bin/ls","ls","-l",NULL); // this is the code the child runs
+			exit(-1);
+		}
+		case -1:
+		{
+			printf("error");
+			exit(-1);
+		} 
+   		default:
+		{
 			wait(&status); // this is the code the parent runs
+			exit(-1);
+		}
    	}
 	return 0;
 }
