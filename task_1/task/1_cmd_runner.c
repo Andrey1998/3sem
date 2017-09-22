f810f1bd873906fc9bae29b4b9feff6ca46c1eee
Andrey1998 <32199925+Andrey1998@users.noreply.github.com>
2017-09-22 23:14:18 +0300

    Update 1_cmd_runner.c

--- task_1/task/1_cmd_runner.c
+++ task_1/task/1_cmd_runner.c
@@ -9,26 +9,74 @@
    a) if execvp is used, string splitting into "path" and "args" is all you need.
 3. Collect exit codes via waitpid/WEXITSTATUS.
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <iostream>
+#include <string.h>
 
-int main(int argc, char** argv, char** envp)
+int main()
 {
+	char** arg = (char**)malloc(100*sizeof(char*));
+	for (int i = 0; i < 100; i++)
+	{
+		arg[i] = (char*)malloc(100*sizeof(char));
+	}
+	char c = 'y';
+	int j = 0;
+	char h;
+	int i = 0;
+	int status;
+	int n;
+	while (c =='y')
+	{
+	c = 0;
         const pid_t pid = fork();
         if (pid == -1) {
                 printf("fork() error\n");
                 return -1;
         }
         if (pid != 0) {
-                int status;
                 waitpid(pid, &status, 0);
                 printf("Ret code: %d\n", WEXITSTATUS(status));
+		printf("Once more?");
+		scanf("%c", &c);
         } else {
-		execve(argv[1], &argv[2], envp);
+		i = 0;
+		printf("args:");
+		scanf("%d ",&n);
+		j = 0;
+		for (i = 0; i < n; i++)
+		{
+			/*
+			scanf("%c", &h);
+			arg[i][j] = h;
+			while (h != '\n')
+			{
+				j++;
+				scanf("%c", &h);
+				arg[i][j] = h;
+			}
+			arg[i]= arg[i] + '\0';
+			for (int k = 0; k <= j+1; k++)
+				printf ("%c", arg[i][k]);
+			printf("%s", arg[i]);
+			j = 0;*/
+			scanf("%s", arg[i]);
+		}
+		arg[n] = NULL;
+		printf("%s", arg[1]);
+		/*
+		arg[0] = "1.out" + '\0';
+		printf("%s", arg[0]);
+		arg[1] = NULL;
+		*/
+		execve(arg[0], arg, NULL);
         }
+	}
         return 0;
 }
